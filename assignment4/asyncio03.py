dd14e1eefbed5962f787aaceb1bea88c3074412b
MyChayak <mind69317@gmail.com>
2024-07-03 11:59:07 +0700

    submit assignment04

--- assignment4/asyncio03.py
+++ assignment4/asyncio03.py
@@ -1,1 +1,23 @@
 # example of gather for many coroutines in a list
+# example of starting many tasks and getting access to all tasks
+import asyncio
+
+# coroutine for a task
+async def task_coro(value):
+    # report a message
+    print(f'>task {value} executing')
+    # sleep for a moment
+    await asyncio.sleep(1)
+
+# coroutine used for the entry point
+async def main():
+    # report a meassage
+    print('main starting')
+    # create many coroutines
+    coros = [task_coro(i) for i in range(10)]
+    # run the tasks
+    await asyncio.gather(*coros)
+    print('main done')
+
+# started the asyncio program
+asyncio.run(main())
